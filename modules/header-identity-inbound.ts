2cba8bdc4b00291a074dfb2d0b88eb9e4fff1776
Sam Preston <sam@zuplo.com>
2026-08-19 12:38:45 -0600

    Diagnostic: include empty data field on request.user to test quotaBy:user

--- modules/header-identity-inbound.ts
+++ modules/header-identity-inbound.ts
@@ -4,6 +4,6 @@ const IDENTITY_HEADER = "x-user-sub";
 
 export default async function (request: ZuploRequest, context: ZuploContext) {
   const sub = request.headers.get(IDENTITY_HEADER) ?? "anonymous";
-  request.user = { sub };
+  request.user = { sub, data: {} };
   return request;
 }
